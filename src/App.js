4ee06377c646cf3347e68c843af9a0bfd78ba702
Aaron Yu <ayybuddyy@gmail.com>
2021-04-01 18:44:13 -0400

    install firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -6,14 +6,21 @@ import {
   Redirect,
 } from 'react-router-dom';
 
-import NewPlace from './places/pages/NewPlace';
-import UpdatePlace from './places/pages/UpdatePlace';
-import UserPlaces from './places/pages/UserPlaces';
+// import Users from './user/pages/Users';
+// import NewPlace from './places/pages/NewPlace';
+// import UpdatePlace from './places/pages/UpdatePlace';
+// import UserPlaces from './places/pages/UserPlaces';
+// import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
-import Auth from './user/pages/Auth';
-import Users from './user/pages/Users';
+import LoadingSpinner from './shared/components/UIElements/LoadingSpinner';
 import { AuthContext } from './shared/context/auth-context';
 import { useAuth } from './shared/hooks/auth-hook';
+
+const Users = React.lazy(() => import('./user/pages/Users'));
+const Auth = React.lazy(() => import('./user/pages/Auth'));
+const NewPlace = React.lazy(() => import('./places/pages/NewPlace'));
+const UpdatePlace = React.lazy(() => import('./places/pages/UpdatePlace'));
+const UserPlaces = React.lazy(() => import('./places/pages/UserPlaces'));
 
 const App = () => {
   const { token, login, logout, userId } = useAuth();
@@ -66,7 +73,17 @@ const App = () => {
     >
       <Router>
         <MainNavigation />
-        <main>{routes}</main>
+        <main>
+          <Suspense
+            fallback={
+              <div className='center'>
+                <LoadingSpinner />
+              </div>
+            }
+          >
+            {routes}
+          </Suspense>
+        </main>
       </Router>
     </AuthContext.Provider>
   );
